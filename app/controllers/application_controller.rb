e17be7f8314c37607dd4cfaaa2e22b1e5e7e1614
learn <learn@learn06-3.localdomain>
2018-12-06 14:23:27 -0800

    devise fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::API
   respond_to :json  # <- Add this line
   include ActionController::MimeResponds # <- Add this line
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user|
+      user.permit(:email, :password, :password_confirmation, :address1, :city, :state, :zipcode)
+    end
+  end
+
 end
