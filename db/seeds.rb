c96da547191cb3b6ee02668f8e8c997c3cec5beb
learn <learn@learn01.localdomain>
2018-12-18 10:54:05 -0800

    changed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,3 @@
-#USER SEEDS
 user1 = User.create!(
   {
     email: 'moderator@peapod.com',
@@ -17,10 +16,10 @@ user2 = User.create!(
     email: 'tester2@gmail.com',
     password: '111111',
     password_confirmation: '111111',
-    address1: '289 6th Ave',
+    address1: '4193 University Ave',
     city: 'San Diego',
     state: 'California',
-    zipcode: '92101'
+    zipcode: '92105'
   }
 )
 
@@ -29,10 +28,10 @@ user3 = User.create!(
     email: 'tester3@gmail.com',
     password: '111111',
     password_confirmation: '111111',
-    address1: '704 J St.',
+    address1: '3288 Adams Ave',
     city: 'San Diego',
     state: 'California',
-    zipcode: '92101'
+    zipcode: '92116'
   }
 )
 
@@ -41,10 +40,10 @@ user4 = User.create!(
     email: 'tester4@gmail.com',
     password: '111111',
     password_confirmation: '111111',
-    address1: '655 W Broadway',
+    address1: '3288 Adams Ave',
     city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
+    state: 'California',
+    zipcode: '92116'
   }
 )
 
@@ -53,10 +52,10 @@ user5 = User.create!(
     email: 'tester5@gmail.com',
     password: '111111',
     password_confirmation: '111111',
-    address1: '101 Market Street',
+    address1: '4740 Mission Gorge Pl',
     city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
+    state: 'California',
+    zipcode: '92120'
   }
 )
 
@@ -65,10 +64,10 @@ user6 = User.create!(
     email: 'tester6@gmail.com',
     password: '111111',
     password_confirmation: '111111',
-    address1: '1770 Village Pl',
+    address1: '3911 Cleveland Ave',
     city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
+    state: 'California',
+    zipcode: '92103'
   }
 )
 
@@ -77,10 +76,10 @@ user7 = User.create!(
     email: 'tester7@gmail.com',
     password: '111111',
     password_confirmation: '111111',
-    address1: '1143 6th Ave',
+    address1: '6519 Bisby Lake Ave',
     city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
+    state: 'California',
+    zipcode: '92119'
   }
 )
 
@@ -89,10 +88,10 @@ user8 = User.create!(
     email: 'tester8@gmail.com',
     password: '111111',
     password_confirmation: '111111',
-    address1: '400 W Broadway',
+    address1: '5505 Stevens Way',
     city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
+    state: 'California',
+    zipcode: '92114'
   }
 )
 
@@ -101,10 +100,10 @@ user9 = User.create!(
     email: 'tester9@gmail.com',
     password: '111111',
     password_confirmation: '111111',
-    address1: '427 C St',
+    address1: '815 E St',
     city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
+    state: 'California',
+    zipcode: '92101'
   }
 )
 
@@ -113,696 +112,53 @@ user10 = User.create!(
     email: 'tester10@gmail.com',
     password: '111111',
     password_confirmation: '111111',
-    address1: '240 Broadway',
+    address1: '6401 El Cajon Blvd',
     city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
+    state: 'California',
+    zipcode: '92115'
   }
 )
 
-user11 = User.create!(
-  {
-    email: 'tester11@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '1230 Columbia St',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
 
-user12 = User.create!(
-  {
-    email: 'tester12@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '655 W Broadway',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user13 = User.create!(
-  {
-    email: 'tester13@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '345 6th Ave',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user14 = User.create!(
-  {
-    email: 'tester14@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '701 B St',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user15 = User.create!(
-  {
-    email: 'tester15@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '1050 Columbia St',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user16 = User.create!(
-  {
-    email: 'tester16@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '777 6th Ave',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user17 = User.create!(
-  {
-    email: 'tester17@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '905 J St',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user18 = User.create!(
-  {
-    email: 'tester18@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '1495 Pacific Hwy',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user19 = User.create!(
-  {
-    email: 'tester19@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '444 W Cedar St',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user20 = User.create!(
-  {
-    email: 'tester20@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '600 B St',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user21 = User.create!(
-  {
-    email: 'tester21@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '600 B St',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user22 = User.create!(
-  {
-    email: 'tester22@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '1009 N Harbor Dr',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user23 = User.create!(
-  {
-    email: 'tester23@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '555 Market St',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user24 = User.create!(
-  {
-    email: 'tester24@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '631 9th Ave',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user25 = User.create!(
-  {
-    email: 'tester25@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '1602 State St',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user26 = User.create!(
-  {
-    email: 'tester26@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '750 B St',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user27 = User.create!(
-  {
-    email: 'tester27@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '421 W B St',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user28 = User.create!(
-  {
-    email: 'tester28@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '430 F St',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user29 = User.create!(
-  {
-    email: 'tester29@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '401 W A St',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user30 = User.create!(
-  {
-    email: 'tester30@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '675 W G St',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user31 = User.create!(
-  {
-    email: 'tester31@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '1068 K St',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user32 = User.create!(
-  {
-    email: 'tester32@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '1301 Market St',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user33 = User.create!(
-  {
-    email: 'tester33@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '1500 State St',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user34 = User.create!(
-  {
-    email: 'tester34@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '1704 India St',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user35 = User.create!(
-  {
-    email: 'tester35@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '181 W Market St',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user36 = User.create!(
-  {
-    email: 'tester36@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '722 11th Ave',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user37 = User.create!(
-  {
-    email: 'tester37@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '829 Garnet Ave',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user38 = User.create!(
-  {
-    email: 'tester38@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '1100 3rd Ave',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user39 = User.create!(
-  {
-    email: 'tester39@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '1100 3rd Ave',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user40 = User.create!(
-  {
-    email: 'tester40@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '472 3rd Ave',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user41 = User.create!(
-  {
-    email: 'tester41@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '160 W Broadway',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user42 = User.create!(
-  {
-    email: 'tester42@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '900 Bayfront Ct',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user43 = User.create!(
-  {
-    email: 'tester43@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '810 13th St',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user44 = User.create!(
-  {
-    email: 'tester44@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '750 W Fir St',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user45 = User.create!(
-  {
-    email: 'tester45@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '2146 Logan Ave',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user46 = User.create!(
-  {
-    email: 'tester46@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '2870 Fourth Ave',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user47 = User.create!(
-  {
-    email: 'tester47@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '2619 National Ave',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user48 = User.create!(
-  {
-    email: 'tester48@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '634 Broadway',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user49 = User.create!(
-  {
-    email: 'tester49@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '1229 9th Ave',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
-
-user50 = User.create!(
-  {
-    email: 'tester50@gmail.com',
-    password: '111111',
-    password_confirmation: '111111',
-    address1: '707 Broadway',
-    city: 'San Diego',
-    state: 'CA',
-    zipcode: '92101',
-  }
-)
 
 #ACTIVITY SEEDS
 activity1 = Activity.create({ name: 'Driving' })
 activity2 = Activity.create({ name: 'Hiking' })
-activity3 = Activity.create({ name: 'Jogging' })
+activity3 = Activity.create({ name: 'Skiing' })
 activity4 = Activity.create({ name: 'GeoCaching' })
 activity5 = Activity.create({ name: 'Gaming' })
 activity6 = Activity.create({ name: 'Biking' })
 activity7 = Activity.create({ name: 'Arts & Crafts' })
 activity8 = Activity.create({ name: 'Stargazing' })
 
-#ACTIVITY#USERS SEEDS
+
+
 user2.activities << activity1
+user2.activities << activity2
 user2.activities << activity3
-user2.activities << activity5
-
-user3.activities << activity2
-user3.activities << activity4
+user3.activities << activity3
+user3.activities << activity5
 user3.activities << activity6
+user3.activities << activity7
 
-user4.activities << activity8
-user4.activities << activity7
+user4.activities << activity4
+user4.activities << activity5
 user4.activities << activity6
+user4.activities << activity7
+user5.activities << activity4
+user5.activities << activity6
+user5.activities << activity7
+user6.activities << activity7
+user6.activities << activity1
+user6.activities << activity2
 
-user5.activities << activity1
-user5.activities << activity2
-user5.activities << activity3
-
-user6.activities << activity4
-user6.activities << activity5
-user6.activities << activity6
-
-user7.activities << activity2
-user7.activities << activity4
-user7.activities << activity5
-
+user7.activities << activity8
+user7.activities << activity6
+user7.activities << activity3
+user8.activities << activity2
+user8.activities << activity4
+user8.activities << activity8
 user8.activities << activity1
-user8.activities << activity3
-user8.activities << activity5
-
-user9.activities << activity8
 user9.activities << activity7
-user9.activities << activity6
-
-user10.activities << activity8
-user10.activities << activity1
+user9.activities << activity5
 user10.activities << activity2
-
-user11.activities << activity8
-user11.activities << activity3
-user11.activities << activity4
-
-user12.activities << activity1
-user12.activities << activity3
-user12.activities << activity4
-
-user13.activities << activity2
-user13.activities << activity4
-user13.activities << activity6
-
-user14.activities << activity7
-user14.activities << activity1
-user14.activities << activity3
-
-user15.activities << activity7
-user15.activities << activity2
-user15.activities << activity4
-
-user16.activities << activity1
-user16.activities << activity3
-user16.activities << activity5
-
-user17.activities << activity5
-user17.activities << activity7
-user17.activities << activity8
-
-user18.activities << activity2
-user18.activities << activity4
-user18.activities << activity6
-
-user19.activities << activity8
-user19.activities << activity7
-user19.activities << activity1
-
-user20.activities << activity1
-user20.activities << activity3
-user20.activities << activity5
-
-user21.activities << activity8
-user21.activities << activity7
-user21.activities << activity6
-
-user22.activities << activity2
-user22.activities << activity4
-user22.activities << activity6
-
-user23.activities << activity1
-user23.activities << activity3
-user23.activities << activity4
-
-user24.activities << activity8
-user24.activities << activity7
-user24.activities << activity6
-
-user25.activities << activity1
-user25.activities << activity2
-user25.activities << activity3
-
-user26.activities << activity4
-user26.activities << activity5
-user26.activities << activity6
-
-user27.activities << activity8
-user27.activities << activity7
-user27.activities << activity6
-
-user28.activities << activity2
-user28.activities << activity4
-user28.activities << activity6
-
-user29.activities << activity1
-user29.activities << activity3
-user29.activities << activity5
-
-user30.activities << activity8
-user30.activities << activity7
-user30.activities << activity6
-
-user31.activities << activity2
-user31.activities << activity3
-user31.activities << activity4
-
-user32.activities << activity3
-user32.activities << activity5
-user32.activities << activity7
-
-user33.activities << activity7
-user33.activities << activity8
-user33.activities << activity1
-
-user34.activities << activity6
-user34.activities << activity2
-user34.activities << activity4
-
-user35.activities << activity1
-user35.activities << activity3
-user35.activities << activity5
-
-user36.activities << activity8
-user36.activities << activity6
-user36.activities << activity4
-
-user37.activities << activity3
-user37.activities << activity5
-user37.activities << activity7
-
-user38.activities << activity4
-user38.activities << activity6
-user38.activities << activity8
-
-user39.activities << activity1
-user39.activities << activity2
-user39.activities << activity3
-
-user40.activities << activity4
-user40.activities << activity5
-user40.activities << activity6
-
-user41.activities << activity8
-user41.activities << activity7
-user41.activities << activity6
-
-user42.activities << activity8
-user42.activities << activity6
-user42.activities << activity4
-
-user43.activities << activity3
-user43.activities << activity5
-user43.activities << activity7
-
-user44.activities << activity1
-user44.activities << activity3
-user44.activities << activity5
-
-user45.activities << activity2
-user45.activities << activity4
-user45.activities << activity6
-
-user46.activities << activity4
-user46.activities << activity6
-user46.activities << activity8
-
-user47.activities << activity7
-user47.activities << activity5
-user47.activities << activity3
-
-user48.activities << activity1
-user48.activities << activity2
-user48.activities << activity3
-
-user49.activities << activity4
-user49.activities << activity5
-user49.activities << activity6
-
-user50.activities << activity6
-user50.activities << activity7
-user50.activities << activity8
